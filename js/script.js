ba3e2d30dd347ce93e8b6457fb5cbd0fb5e5d68b
Hellen Silva <127620071+hellen-silvaa@users.noreply.github.com>
2024-12-08 22:40:26 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// ===== Turns the menu into a X =====
+
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+menuHamburguer.addEventListener('click', () => {
+    toggleMenu();
+});
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuHamburguer.classList.toggle('change');
+
+    if (menuHamburguer.classList.contains('change')) {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+    this.sendForm = this.sendForm.bind(this);
+}
